9d59d2f45373052118fcfcca7d2514984a64ae72
sumkad <sumanthk@seas.upenn.edu>
2023-01-31 18:48:29 -0500

    Finished HW 1

--- assignments/week1/model.py
+++ assignments/week1/model.py
@@ -1,30 +1,34 @@
 import numpy as np
+import random
 
 
+"""
+    A linear regression model that uses gradient descent to fit the model.
+"""
 class LinearRegression:
-
+    """
+        A linear regression model that uses gradient descent to fit the model.
+    """
     w: np.ndarray
     b: float
+    def __init__(self) -> None:
+        # raise NotImplementedError()
+        self.w = None
+        self.b = None
 
-    def __init__(self):
-        raise NotImplementedError()
+    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+        """
+        Predict the output for the given input.
 
-    def fit(self, X, y):
-        raise NotImplementedError()
-
-    def predict(self, X):
-        raise NotImplementedError()
-
-
-class GradientDescentLinearRegression(LinearRegression):
-    """
-    A linear regression model that uses gradient descent to fit the model.
-    """
-
-    def fit(
-        self, X: np.ndarray, y: np.ndarray, lr: float = 0.01, epochs: int = 1000
-    ) -> None:
-        raise NotImplementedError()
+        Arguments:
+            X (np.ndarray): The input data.
+        """
+        biasX = np.concatenate((X, np.ones((X.shape[0], 1))), axis=1)
+        print(biasX)
+        biasW = np.linalg.pinv((biasX.T @ biasX)) @ biasX.T @ y
+        print(biasW)
+        self.b = biasW[-1]
+        self.w = biasW[:-1]
 
     def predict(self, X: np.ndarray) -> np.ndarray:
         """
@@ -35,6 +39,40 @@ class GradientDescentLinearRegression(LinearRegression):
 
         Returns:
             np.ndarray: The predicted output.
-
         """
-        raise NotImplementedError()
+        return X @ self.w + self.b
+
+
+class GradientDescentLinearRegression(LinearRegression):
+    """
+    A linear regression model that uses gradient descent to fit the model.
+    """
+
+    def fit(
+        self, X: np.ndarray, y: np.ndarray, lr: float = 0.01, epochs: int = 1000
+    ) -> None:
+        """
+        Predict the output for the given input.
+
+        Arguments:
+            X (np.ndarray): The input data.
+        """
+        self.w = np.array([[random.uniform(-0.001, 0.001)] for i in range(X.shape[1])])
+        self.b = 0
+        for epoch in range(epochs):
+            guesses = X @ self.w + self.b
+            err = y - guesses
+            self.w = self.w + lr * (X.T @ err)
+            self.b = self.b + lr * np.sum(err)
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Predict the output for the given input.
+
+        Arguments:
+            X (np.ndarray): The input data.
+
+        Returns:
+            np.ndarray: The predicted output.
+        """
+        return X @ self.w + self.b
