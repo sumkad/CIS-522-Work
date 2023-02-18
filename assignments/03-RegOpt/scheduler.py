6f60ce3c0aaa2808a0d75b5b545ffd85363a0590
sumkad <sumanthk@seas.upenn.edu>
2023-02-18 12:37:36 -0500

    LR Attempt

--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -4,21 +4,41 @@ from torch.optim.lr_scheduler import _LRScheduler
 
 
 class CustomLRScheduler(_LRScheduler):
+    """
+    Custom LR optimizer
+
+    """
     def __init__(self, optimizer, last_epoch=-1):
         """
         Create a new scheduler.
 
-        Note to students: You can change the arguments to this constructor,
-        if you need to add new parameters.
+        Arguments:
+            optimizer: the optimizer used to modify the floats
+            last_epoch: The last epoch of iteration
 
         """
         # ... Your Code Here ...
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
+        self.lastStep = 0
 
     def get_lr(self) -> List[float]:
+        """
+        Returns the learning rate
+        """
+        return [i for i in self.base_lrs]
+
+    def step(self, epoch = None) -> None:
+        """
+        Exponentially decreases the learning rate over time
+
+        Arguments:
+            epoch: the epoch of iteration
+        """
         # Note to students: You CANNOT change the arguments or return type of
         # this function (because it is called internally by Torch)
-
         # ... Your Code Here ...
         # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+        self.lastStep += 1
+        if self.lastStep % 25 == 0:
+            self.base_lrs *= 0.8
+
